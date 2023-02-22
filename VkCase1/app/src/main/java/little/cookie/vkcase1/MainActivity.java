bc55e6b930572880178b368c052fab692b247759
im-personal <Ilia-gaprindashvili@yandex.ru>
2023-02-22 11:38:22 +0300

    Final
    Blur added, pfps rounded, faces fixed

--- VkCase1/app/src/main/java/little/cookie/vkcase1/MainActivity.java
+++ VkCase1/app/src/main/java/little/cookie/vkcase1/MainActivity.java
@@ -10,6 +10,13 @@ import androidx.transition.TransitionManager;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
@@ -77,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
     {
         ImageView us1 = findViewById(R.id.blurView1);
         Bitmap bitmap = ((BitmapDrawable)us1.getDrawable()).getBitmap();
-        Blurry.with(this).radius(25).sampling(2).from(bitmap).into(us1);
+        Blurry.with(this).radius(25).sampling(2).from(bitmap.copy(bitmap.getConfig(), true)).into(us1);
 
         ImageView us2 = findViewById(R.id.blurView2);
         bitmap = ((BitmapDrawable)us2.getDrawable()).getBitmap();
@@ -245,11 +252,29 @@ public class MainActivity extends AppCompatActivity {
         user2 = findViewById(R.id.user2_place);
         usersPlace = findViewById(R.id.usersplace);
 
-        pfp_user1 = findViewById(R.id.pfp1);
-        pfp_user2 = findViewById(R.id.pfp2);
-
     }
 
+    private static Bitmap getRoundedCornerBitmap(Bitmap bitmap, int pixels) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap
+                .getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = pixels;
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
+    }
 
 
 }
